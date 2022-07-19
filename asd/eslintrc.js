9908e69bd83a636e52523aae2e6ac5ca54999a0c
Jang Gyeoungguk <97073355+rnr9928@users.noreply.github.com>
2022-07-19 12:45:15 +0900

    Add files via upload
    fomatting
    eslint
    를 배웠다

---
+++ asd/eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: ["airbnb-base", "plugin:node/recommended", "prettier"],
+};
+
+// air bnb 패키지 설치 명령어
+// npm install --save-dev eslint-config-airbnb-base
+// npm install --save-dev eslint-plugin-import
+// npm 패키지 두개 다운
+// npm install --save-dev eslint-config-airbnb-base eslint-plugin-import
+
+// eslint prettier package 다운
+// npm install --save-dev eslint-config-prettier
+
+// node 전용 플러그인 설치
+
+// npm install --save-dev eslint-plugin-node
