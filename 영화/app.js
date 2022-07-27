109130a2d36490f3bef0117c2a9133b99ce23db9
Jang Gyeoungguk <97073355+rnr9928@users.noreply.github.com>
2022-07-27 17:27:32 +0900

    Add files via upload

--- 영화/app.js
+++ 영화/app.js
@@ -1,49 +1,57 @@
+//영화관 좌석 예약 만들기
+
 // 사용할 모듈
-// socketio , express , fs
+// socket.io, express, fs, nodemon
 
-const socketio = require('socket.io');
-const express = require('express');
-const fs  = require('fs');
+/*
+설치 명령어
+npm i express
+npm i socket.io
+노드몬은 글로벌이라 괜츈
+*/
 
-
+const express = require("express");
+const socketio = require("socket.io");
+const fs = require("fs");
 
 let seats = [
-        [1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1],
-        [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
-        [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
-        [1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1],
-        [1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1],
-        [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
-        [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
-        [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
-        [1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1],
-        [1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1],
-        [1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1],
-        [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
-      ];
-      
+    [1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1],
+    [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+    [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+    [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+    [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+    [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+    [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+    [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+    [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+    [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+    [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+    [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+];
 
-      const app = express();
-      const PORT = 3000;
-      const server = app.listen(PORT, () => {
-        console.log(PORT,"번 포트 실행");
-      });
+// 웹 서버 생성
+const app = express();
+const PORT = 3000;
+const server = app.listen(PORT, () => {
+    console.log(PORT, "번 포트 실행");
+});
 
-      const io = socketio(server);
+// socket.io 생성 및 실행
+const io = socketio(server);
 
-      app.get("/", (req,res)=>{
-        fs.readFile('page.html',(err,data)=>{
-            res.send(data.toString());
-        });
-      });
+app.get("/", (req, res) => {
+    fs.readFile("page.html", (err, data) => {
+        res.send(data.toString());
+    });
+});
 
-      app.get('/seats',(req,res)=>{
-        res.send(seats);
-      });
+app.get("/seats", (req, res) => {
+    res.send(seats);
+})
 
-      io.sockets.on('connection',(socket)=>{
-        socket.on('reserve',(data)=>{
-            seats[data.y][data.x]= 2;
-            io.sockets.emit('reserve', data);
-        });
-      });
+io.sockets.on("connection", (socket) => {
+    socket.on("reserve", (data) => {
+        seats[data.y][data.x] = 2;
+        io.sockets.emit("reserve", data);
+    });
+});
